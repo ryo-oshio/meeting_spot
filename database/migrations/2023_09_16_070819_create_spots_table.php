7d82566e1720d68b3dc0e3f77f0f1cac636f3108
ryo-oshio <syuukatsu0128@gmail.com>
2023-09-17 15:06:02 +0900

    migrateionファイル作成

---
+++ database/migrations/2023_09_16_070819_create_spots_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('body', 2000);
+            $table->char("congetion",10);
+            $table->char("access",10);
+            $table->char("minute",10);
+            $table->string('image', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+};
